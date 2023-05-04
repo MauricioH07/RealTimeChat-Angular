361bbe2a350ecc431613596cae8e9b112cb1325f
David Pérez Villanueva <david@grupoiris.com>
2023-05-04 20:12:36 +0200

    Añadida la configuración del backend al fichero de environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
+    production: false,
     path:'/chats',
     firebase: {
         apiKey: "AIzaSyDFQkk0ikYKzkTsBcINXqI_wlF6BhI0e3E",
@@ -9,5 +10,6 @@ export const environment = {
         messagingSenderId: "450151373178",
         appId: "1:450151373178:web:57463feb34a8642264dec3",
         measurementId: "G-79K1LLH63W"
-    }
+    },
+    swiftbackServer: "http://localhost:8080"
 };
